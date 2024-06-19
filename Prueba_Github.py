d170715f479a11d9a521d15d58e983e1d2c58684
Vicentiin18 <vice.alvarezm@duocuc.cl>
2024-06-19 11:52:12 -0400

    Update Prueba_Github.py
    ingresar numero 5

--- Prueba_Github.py
+++ Prueba_Github.py
@@ -1,4 +1,14 @@
 Saludo = input ("Saluda")
 if Saludo == "Hola" or Saludo == "hola"
   print("Hola Chorombo")
-print ("Hola Chorombo"
+print ("Hola Chorombo")
+
+
+
+print("ingresa un numero")
+numero=int(input())
+if numero==5:
+  
+
+
+
