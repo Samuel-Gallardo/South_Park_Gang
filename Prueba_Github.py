963fbfb8225d85d55b977fba483bdac16e6dff40
Samuel-Gallardo <sa.gallardov@duocuc.cl>
2024-06-19 12:03:38 -0400

    Update Prueba_Github.py

--- Prueba_Github.py
+++ Prueba_Github.py
@@ -1,12 +1,16 @@
-import random
-numero_random=random.randint(1,10)
-print("ingresa un numero")
-numero=int(input())
-if numero==numero_random:
-  print("JAJAJA, Escogiste el numero random, que gay")
-else:
-  print("Te Salvaste brother")
-  
+while True:
+  import random
+  numero_random=random.randint(1,10)
+  print("\nIngresa un numero del 1 al 10")
+  numero=int(input())
+  if numero==numero_random:
+    print("JAJAJA, Escogiste el numero random, que gay")
+  else:
+    print("Te Salvaste brother")
+    print(f"El numero era {numero_random}")
+    
+
+
 
 
 
